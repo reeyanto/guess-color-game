3b049465e2cc011a78e077e61a46b5734a9f8ddd
candradwicahyo <candradwicahyo18@gmail.com>
2023-02-01 03:34:52 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Guess Color Game
+
+sebuah game sederhana dengan nama guess color atau game tebak warna yang dibuat menggunakan html, css dan javascript. di game ini, kalian diharuskan menebak sebuah warna yang cocok dengan kode warna yang ada di bagian input. kalian akan diberikan 3 kali kesempatan untuk menebak warna tersebut. jika kalian gagal menebak warna yang benar sebanyak 3 kali, maka kalian akan dianggap kalah.
+
+## dibuat dengan berikut 
+
+* html   
+* css
+* javascript 
+
+## framework
+
+* [bootstrap](https://getbootstrap.com)
+
+## kontributor 
+
+* candra dwi cahyo 
+
+  * [email](candradwicahyo18@gmail.com)
+  * [instagram](https://instagram.com/candradwicahyo18)
+  * [facebook](https://facebook.com/candradwicahyo18)
+
+## preview dan live demo 
+
+![preview](https://github.com/candradwicahyo/guess-color-game/blob/master/image.jpg)
+
+[tekan link ini](https://candradwicahyo.github.io/guess-color-game) untuk mencoba demo program ini
---
+++ script.js
@@ -0,0 +1,70 @@
+window.onload = () => {
+  
+  let heart = 3;
+  
+  const input = document.querySelector('.input');
+  const boxes = document.querySelectorAll('.box');
+  
+  function startGames() {
+    const color = hexColorGenerator();
+    input.value = color;
+    setColorBox(color);
+    return color;
+  }
+  
+  let result = startGames();
+  
+  function hexColorGenerator() {
+    let str = '0123456789abcdef';
+    let result = '#';
+    for (let i = 0; i < 6; i++) {
+      result += str[Math.floor(Math.random() * str.length)];
+    }
+    return result.toLowerCase();
+  }
+  
+  function setColorBox(color) {
+    boxes.forEach(box => {
+      box.style.background = hexColorGenerator();
+      box.setAttribute('data-color', hexColorGenerator());
+    });
+    
+    const box = boxes[Math.floor(Math.random() * boxes.length)];
+    box.style.background = color;
+    box.setAttribute('data-color', color);
+  }
+  
+  boxes.forEach(box => {
+    box.addEventListener('click', function() {
+      const data = this.dataset.color;
+      const check = getResult(data, result);
+      if (check == true) alert(`your heart : ${heart} times again!`);
+      if (heart == 0) {
+        alert('game over! you lose!');
+        playAgain();
+      }
+    });
+  });
+  
+  function getResult(color, result) {
+    if (color == result) {
+      alert(`congratulation! the right color is ${color}`);
+      playAgain();
+    } else if (color != result) {
+      alert('wrong color! please try again!');
+      heart--;
+      return true;
+    }
+  }
+  
+  function playAgain() {
+    const ask = confirm('do you want to play again?');
+    if (ask == true) {
+      heart = 3;
+      result = startGames();
+    } else {
+      alert('thanks for playing with us!');
+    }
+  }
+  
+}
